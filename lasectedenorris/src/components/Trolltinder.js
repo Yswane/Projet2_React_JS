a95b297b2895247306b427949ed3c384a3968571
Esoriak <somahaut.dev@gmail.com>
2019-04-04 10:46:48 +0200

    [update] add Troll page of tinder button

---
+++ lasectedenorris/src/components/Trolltinder.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Trolltinder.css'
+
+
+const Tindertroll = () => (
+    <div className="background_tinder">
+        <div className="square"> 
+        <p className="Fuck_it"> 
+        Chuck Norris peut faire du feu en frottant deux glaçons. 
+        </p>
+        <div className="gif_troll_tinder">
+        <iframe className="doggy" src="https://giphy.com/embed/IxdQbEQ33viog" width="480" height="392" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <p className="tinder_norris"> TU VEUX TINDER NORRIS ?<br/>
+        Vas t'en le phoque !</p>
+           <img className="chuck_kick" src="http://www.pngmart.com/files/2/Chuck-Norris-PNG-Photos.png" alt="chuck_kick" />
+            <iframe className="phoquy" src="https://giphy.com/embed/Dym2LTuxGJJSw" width="480" height="402" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/rainbow-vomit-walrus-Dym2LTuxGJJSw"></a></p>
+        </div>
+        </div> 
+       
+        <p className="Move">
+        Il n'a pas besoin de toi pour le chauffer.
+        </p>
+          </div>
+)
+
+export default Tindertroll
