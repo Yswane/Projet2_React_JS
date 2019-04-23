b5cd07b440845e31adb8d0d52bad3c3933d3a389
Esoriak <somahaut.dev@gmail.com>
2019-04-23 15:26:42 +0200

    create new branch and component game

---
+++ lasectedenorris/src/components/Basegame.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class Basegame extends Component {
+
+render ()
+
+{
+    return (
+
+
+        <> <div>        const Question1 = ' Bienvenue, êtes vous prets ?';
+    const reponse1a= 'oui'
+    const reponse1b= 'je m'excuse d'être ici, je me suis trompé d'endroit'
+    const reponse1c= 
+    const Question2a = 'Chuck Norris est en face de toi , il te sourit, que fais-tu ?'
+
+
+
+
+    
+    </div>
+        
+        </>
+
+    )
+}
+
+}
+
+export default Basegame
