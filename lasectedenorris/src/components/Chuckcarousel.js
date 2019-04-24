4edcde760f15b0c41566755629ef680e2faedf2e
Yous <ysouane0@gmail.com>
2019-04-24 14:05:31 +0200

    WIP

--- lasectedenorris/src/components/Chuckcarousel.js
+++ lasectedenorris/src/components/Chuckcarousel.js
@@ -2,21 +2,45 @@ import React from "react";
 import './Chuckcarousel.css';
 
 const Chuckcarousel = ({ picture, picture1, picture2, picture3 }) => {
-
-  return <div className="slider">
-    <div className="slides">
-      <div className="slide">
-        <img src={picture} alt={picture} />
-        <img src={picture1} alt={picture} />
-        <img src={picture2} alt={picture} />
-        <img src={picture3} alt={picture} />
-      </div>
-    </div>
+  return <div id="slider">
+    <figure>
+      <img src={picture} alt={picture} />
+      <img src={picture1} alt={picture} />
+      <img src={picture2} alt={picture} />
+      <img src={picture3} alt={picture} />
+    </figure>
   </div>
 
- 
 };
 
 
 
 export default Chuckcarousel;
+
+
+
+// origine :
+// return <div className="slider">
+//     <div className="slides">
+//       <div className="slide">
+//         <img src={picture} alt={picture} />
+//         <img src={picture1} alt={picture} />
+//         <img src={picture2} alt={picture} />
+//         <img src={picture3} alt={picture} />
+//       </div>
+//     </div>
+//   </div>
+
+
+// modif 1
+
+// return <div id="slider">
+//     <figure>
+//       <img src="{picture}" alt={picture} />
+//       <img src="{picture1}" alt={picture} />
+//       <img src="{picture2}" alt={picture} />
+//       <img src="{picture3}" alt={picture} />
+//     </figure>
+//   </div>
+
+// };
