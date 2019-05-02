a5100b1eaaf51e59a5fec7074fc0817c5f71642c
Elayache Farouk <fa.elayache1@gmail.com>
2019-05-02 16:03:25 +0200

    text change chuck banner

--- lasectedenorris/src/components/Chuckbanner.js
+++ lasectedenorris/src/components/Chuckbanner.js
@@ -19,11 +19,7 @@ class Chuckbanner extends Component {
       <img src="https://image.noelshack.com/fichiers/2019/14/2/1554217990-chuck1.png" alt="picture_chuck"></img>
       </div> 
       <div className="text_ban1">
-      <p>Chuck Norris is my Homeboy Chuck Norris is the only man to ever defeat a brick wall in a game of tennis.
-
-      Chuck Norris is currently suing NBC, claiming Law and Order are trademarked names for his left and right legs Chuck Norris died 20 years ago, Death just hasn't built up the courage to tell him yet Chuck Norris quite likes Chris.
-
-      When Chuck Norris does a pushup, he isn't lifting himself up, he's pushing the Earth down, The chief export of Chuck Norris is Pain. Contrary to popular belief, Chuck Norris, not the box jellyfish of northern Australia, is the most venomous creature on earth, Fool me once, shame on you. Fool Chuck Norris once and he will roundhouse you in the face, Someone once videotaped Chuck Norris getting pissed off.</p>
+      <p>Chuck Norris est devenu malgré lui un meme sur Internet. De très nombreux aphorismes humoristiques, les « Chuck Norris Facts », lui attribuent en effet des qualités surhumaines, caricaturant les héros forts et virils qu'il a interprétés au cinéma et à la télévision. Ces courts énoncés de ses hauts faits vont même jusqu'à présenter comme évidente la supériorité de Chuck Norris sur Dieu (par exemple : « Dieu a dit : "Que la lumière soit !" et Chuck Norris a répondu : "On dit : s'il vous plait !" »). Les thèmes abordés sont très variés, et certains ensembles de facts font référence à des connaissances pointues, ayant généralement été conçus par des étudiants spécialisés dans la discipline correspondante. </p>
       <a href="/Chuck" Target="_blank" className="button-primary1" title="Learn More"><Link to="/Chuck" />Chuck facts <span className="icon-play"><i className="fas fa-chevron-circle-right"></i></span></a>
       </div>
       </div>
